8bbe5853fd99facb03958116e10ba22a1893012f
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-08-31 00:13:51 -0400

    wrong path for component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
-import useDropdown from "./useDropdown";
+import useDropdown from "./use-dropdown";
 import Results from "./Results";
 
 const SearchParams = () => {
